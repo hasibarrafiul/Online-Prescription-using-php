2290f21821f947af5ab4e802d68bfde98446e8b4
gl1tchy_af <fahad.com66.fk@gmail.com>
2022-08-01 19:32:46 +0600

    medicine show

--- home.php
+++ home.php
@@ -14,6 +14,48 @@ else{
 <body>
     <button onclick="window.location.href='login.php'">Logout</button> <br><br><br>
     <button onclick="window.location.href='profile.php'">User Profile</button> <br>
-    <button onclick="window.location.href='insertmedicine.php'">Select Medicine</button> <br>
+    <button onclick="window.location.href='insertmedicine.php'">Select Medicine</button> <br><br><br>
+
+    <h1>Medicine List</h1>
+
+    <?php
+    $dbservername = "localhost";
+    $dbusername = "root";
+    $dbpassword = "";
+    $dbname = "onlineprescription";
+    $conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    $sql = "SELECT * from medicines";
+    $res = mysqli_query($conn, $sql);
+    if (mysqli_num_rows($res) > 0) {
+        $num = 1;
+        echo "<table border='1'>";
+        echo "<tr>";
+        echo "<th>Number</th>";
+        echo "<th>Medicine Name</th>";
+        echo "<th>Medicine Indication</th>";
+        echo "<th>Medicine Usage</th>";
+        echo "<th>Medicine Instruction</th>";
+        echo "</tr>";
+        while($row = mysqli_fetch_assoc($res)) {
+            echo "<tr>";
+            echo "<td>".$num."</td>";
+            echo "<td>".$row["name"]."</td>";
+            echo "<td>".$row["indication"]."</td>";
+            echo "<td>".$row["usages"]."</td>";
+            echo "<td>".$row["instruction"]."</td>";
+            echo "</tr>";
+            $num++;
+        }
+        echo "</table>";
+        
+    } else {
+        echo "0 results";
+    }
+    mysqli_close($conn);
+    ?>
+
 </body>
 </html>
