b72304aadce902ea94694a88ae1171484453868c
gl1tchy_af <fahad.com66.fk@gmail.com>
2022-08-28 17:52:29 +0600

    database

--- onlineprescription.sql
+++ onlineprescription.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 05, 2022 at 08:16 AM
+-- Generation Time: Aug 28, 2022 at 01:52 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.29
 
@@ -40,9 +40,8 @@ CREATE TABLE `medicines` (
 --
 
 INSERT INTO `medicines` (`id`, `name`, `indication`, `usages`, `instruction`) VALUES
-(4, 'med2', '2', '2', '2'),
-(5, 'Napa', 'Antibiotic', 'Eat', 'Daily 2 times'),
-(6, 'test', 'g', 'gg', 'ggg');
+(5, 'Napa', 'Antibiotic', 'Eat and drink', 'Daily 2 times'),
+(9, 'Monas 10 mg', 'Tablet', 'Eat', ' Once daily in the evening');
 
 -- --------------------------------------------------------
 
@@ -64,8 +63,9 @@ CREATE TABLE `patientinfo` (
 --
 
 INSERT INTO `patientinfo` (`id`, `name`, `gender`, `age`, `address`, `number`) VALUES
-(1, 'test', 'test', 55, 'tesat', 55),
-(3, 'Hasib', 'Male', 23, 'Dhaka', 1794798101);
+(1, 'Fahim', 'Male', 23, 'Banasree, Dhaka', 1941100584),
+(3, 'Hasib', 'Male', 23, 'Dhaka', 1794798101),
+(4, 'Safa', 'Female', 22, 'Mirpur, Dhaka', 1992339969);
 
 -- --------------------------------------------------------
 
@@ -89,7 +89,8 @@ CREATE TABLE `prescribedmedicines` (
 --
 
 INSERT INTO `prescribedmedicines` (`id`, `prescribedby`, `prescribedto`, `med1`, `med2`, `med3`, `med4`, `med5`) VALUES
-(8, 11, 3, 5, 0, 0, 0, 0);
+(10, 15, 3, 5, 9, 5, 9, 9),
+(11, 15, 1, 5, 9, 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -103,6 +104,7 @@ CREATE TABLE `profile` (
   `name` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `email` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `phonenumber` int(11) DEFAULT NULL,
+  `specialist` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `address` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
   `file_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `uploaded_on` datetime NOT NULL,
@@ -113,8 +115,11 @@ CREATE TABLE `profile` (
 -- Dumping data for table `profile`
 --
 
-INSERT INTO `profile` (`id`, `username`, `name`, `email`, `phonenumber`, `address`, `file_name`, `uploaded_on`, `status`) VALUES
-(5, 'fahim', 'TosTech LLC2', 'tostechllc@gmail.com2', 155555, 'Mawna, Gazipur2', 'Inked272001081_460916608833977_9038083766458093241_n.jpg', '2022-08-05 12:15:30', '1');
+INSERT INTO `profile` (`id`, `username`, `name`, `email`, `phonenumber`, `specialist`, `address`, `file_name`, `uploaded_on`, `status`) VALUES
+(6, 'fahim', 'TosTech LLC', 'tostechllc@gmail.com', 1794798101, '', 'Mawna, Gazipur', 'logo1flip.jpg', '2022-08-05 12:17:47', '1'),
+(7, 'admin', 'Hasib Ar Rafiul Fahim', 'tostechllc@gmail.com', 1794798101, '', 'Gazipur', 'logo1flip.jpg', '2022-08-06 15:29:29', '1'),
+(10, 'Safa', 'Noor Fabi Shah', 'safashah@gmail.com', 1992339969, 'Human Heart', 'Mirpur, Dhaka', '297475355_2323829667764967_8580462172583336594_n.jpg', '2022-08-09 20:42:55', '1'),
+(16, 'sumit', 'fwa', 'ff@g', 4651, 'afa', 'faf', 'adada.png', '2022-08-18 18:48:55', '1');
 
 -- --------------------------------------------------------
 
@@ -133,7 +138,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(11, 'fahim', 'b94c5dbc799331f0ee036db0c145b5438b9a39dc6984ca5fa1260ca0170df32b');
+(11, 'fahim', 'b94c5dbc799331f0ee036db0c145b5438b9a39dc6984ca5fa1260ca0170df32b'),
+(12, 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'),
+(14, 'fahimmars', 'b94c5dbc799331f0ee036db0c145b5438b9a39dc6984ca5fa1260ca0170df32b'),
+(15, 'Safa', '1d6717d20624c7d6dca15e40625edb3135f6710d60f48f22d84e7e5b9ab7464a'),
+(16, 'sumit', '3caeb1fe9939783e2a8f3649df883ff935b2e4bfa66cd08bc258e169f686d692');
 
 --
 -- Indexes for dumped tables
@@ -178,31 +187,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `medicines`
 --
 ALTER TABLE `medicines`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `patientinfo`
 --
 ALTER TABLE `patientinfo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `prescribedmedicines`
 --
 ALTER TABLE `prescribedmedicines`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `profile`
 --
 ALTER TABLE `profile`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
